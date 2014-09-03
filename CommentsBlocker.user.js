d70c08cac989bb7616034c7c0e1edf509954f645
Vítor Galvão <info@vitorgalvao.com>
2014-09-03 16:16:25 +0100

    hide annotations separated from share panel loop

--- CommentsBlocker.user.js
+++ CommentsBlocker.user.js
@@ -16,24 +16,19 @@ if (location.hostname == "www.youtube.com") {
   // show full video description
   document.querySelector("#action-panel-details").className="action-panel-content yt-card yt-card-has-padding yt-uix-expander";
 
-  // show share panel and hide annotations
-  function share_annotations() {
-    // open share panel
-    document.querySelectorAll(".action-panel-trigger")[1].click();
-    // hide annotations
-    if (document.querySelector(".ytp-segmented-control-other")) { // this will only be true if the video has annotations
-      document.querySelector("#settings_button").click();
-      document.querySelector(".ytp-segmented-control-other").click();
-      document.querySelector("#settings_button").click();
-    }
+  // hide annotations
+  if (document.querySelector(".ytp-segmented-control-other")) { // this will only be true if the video has annotations
+    document.querySelector("#settings_button").click();
+    document.querySelector(".ytp-segmented-control-other").click();
+    document.querySelector("#settings_button").click();
   }
 
-  // try to run share_annotations() until share panel is visible
-  share_annotations_loop = window.setInterval(function() {
-    if (document.querySelector("#watch-action-panels").offsetWidth == 0) { // if share panel is not visible on page
-      share_annotations();
+  // show share panel
+  show_share_panel_loop = window.setInterval(function() {
+    if (document.querySelector("#watch-action-panels").offsetWidth == 0) { // if share panel is not yet visible on page
+      document.querySelectorAll(".action-panel-trigger")[1].click(); // try to show it
     } else {
-      clearInterval(share_annotations_loop);
+      clearInterval(show_share_panel_loop);
     }
   }, 500);
 }
