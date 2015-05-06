02945a2ce55b83104dd7f1f27e10dc9e83a3ac03
Vítor Galvão <info@vitorgalvao.com>
2015-05-06 02:25:43 +0100

    use settimeout

--- YoutubeChanges.user.js
+++ YoutubeChanges.user.js
@@ -2,10 +2,12 @@
 // @match http://*.youtube.com/*
 // ==/UserScript==
 
-// hide comments
-document.querySelector("#watch-discussion").style.display = "none";
-// show full video description
-document.querySelector("#action-panel-details").className="action-panel-content yt-card yt-card-has-padding yt-uix-expander";
+window.setTimeout(function() {
+  // hide comments
+  document.querySelector("#watch-discussion").style.display = "none";
+  // show full video description
+  document.querySelector("#action-panel-details").className="action-panel-content yt-card yt-card-has-padding yt-uix-expander";
 
-// hide annotations
-document.querySelector(".ytp-segmented-control-other").click();
+  // hide annotations
+  document.querySelector(".ytp-segmented-control-other").click();
+}, 2000);
