e22a5f82b03facd09da0e4703113fb1045d97629
Vítor Galvão <info@vitorgalvao.com>
2015-08-04 01:13:01 +0100

    YoutubeChanges.user.js: updates for new youtube player

--- YoutubeChanges.user.js
+++ YoutubeChanges.user.js
@@ -1,25 +1,32 @@
 // ==UserScript==
 // @name          YouTubeChanges
 // @namespace     http://vitorgalvao.com/
-// @version       0.1
+// @version       0.1.1
 // @description   On Youtube, turns off annotations and expands the description box.
 // @match         https://*.youtube.com/watch*
 // @run-at        document-end
 // ==/UserScript==
 
+function open_close_settings() {
+  "use strict";
+
+  var settings_button = document.querySelector(".ytp-settings-button");
+  settings_button.click();
+  settings_button.click();
+}
+
 function change_youtube() {
+  "use strict";
+
   // show full video description
-  var expand_description = document.getElementsByClassName("yt-uix-expander-collapsed-body")[0];
-  if (expand_description.querySelector("span").innerHTML == "Show more") {
-    expand_description.click();
-  }
+  document.getElementById("action-panel-details").className = document.getElementById("action-panel-details").className.replace(/(?:^|\s)yt-uix-expander-collapsed(?!\S)/g, "");
 
   // hide annotations
-  var annotations_off = document.querySelector("div[aria-labelledby='ytp-menu-iv']");
-  if (annotations_off) {
-    annotations_off.click();
+  var annotations_label = document.getElementsByClassName("ytp-menuitem-label")[1];
+  if (annotations_label.innerHTML === "Annotations") {
+    annotations_label.click();
   }
 }
 
-change_youtube();
+open_close_settings(); // needs to be done when first opening a youtube video, so the annotations label gets created
 document.getElementById("player-api").addEventListener("focus", change_youtube); // fires when video changes
